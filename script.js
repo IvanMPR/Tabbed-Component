4dbdacfd80b39de5b1273fa0cae5e50846ec12c7
Ivan MPR <ivsanmas@gmail.com>
2021-10-15 16:04:09 +0200

    second active class added

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 'use-strict';
+
 const contentContainer = document.querySelector('.tc-container');
 const contentDivs = document.querySelectorAll('.content');
 const btnContainer = document.querySelector('.buttons-container');
 const buttons = document.querySelectorAll('.btn');
 
+// Make first tab active by default
 function init() {
   contentDivs.forEach(div => div.classList.add('hidden'));
   document.querySelector('.content[data-num="0"]').classList.remove('hidden');
-  document.querySelector('.btn[data-btn="0"]').classList.add('btn-active');
+  //prettier-ignore
+  document.querySelector('.btn[data-btn="0"]').classList.add('btn-active', 'btn-0-active');
 }
 init();
 
@@ -15,18 +18,17 @@ btnContainer.addEventListener('click', function (e) {
   if (!e.target.classList.contains('btn')) return;
   const num = e.target.dataset.btn;
   const currentBtn = document.querySelector(`.btn[data-btn="${num}"]`);
-  const currentColor = window
-    .getComputedStyle(currentBtn)
-    .getPropertyValue('background-color');
+  //prettier-ignore
+  const currentColor = window.getComputedStyle(currentBtn).getPropertyValue('background-color');
   //  Change border color on parent container
   contentContainer.style.border = `2px solid ${currentColor}`;
   // Remove active class from all btn-s and add it to active one
-  buttons.forEach(btn => btn.classList.remove('btn-active'));
+  //prettier-ignore
+  buttons.forEach(btn => btn.classList.remove('btn-active','btn-0-active', 'btn-1-active', 'btn-2-active'));
   currentBtn.classList.add('btn-active');
-
+  currentBtn.classList.add(`btn-${num}-active`);
   // Add hidden class to all content divs and remove it from active one
   contentDivs.forEach(div => div.classList.add('hidden'));
-  document
-    .querySelector(`.content[data-num="${num}"`)
-    .classList.remove('hidden');
+  //prettier-ignore
+  document .querySelector(`.content[data-num="${num}"`).classList.remove('hidden');
 });
